24c3aaf50c5e2d663bccd7f3aa9f0d4465f52d50
mokira3d48 <arnoldmokira3d48@gmail.com>
2025-11-07 17:44:28 +0100

    Spliting of optimizer operations from scheduler operation into defferent modules.

--- src/cva_net/classification/scheduler.py
+++ src/cva_net/classification/scheduler.py
@@ -12,69 +12,69 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(levelname)s \t %(message)s',
     handlers=[
-        logging.FileHandler("classification_optimizer.log"),
+        logging.FileHandler("classification_scheduler.log"),
         logging.StreamHandler()
     ]
 )
 LOGGER = logging.getLogger(__name__)
 
 
+
 ###############################################################################
-# OPTIMIZER
+# OPTIMIZATION SCHEDULER
 ###############################################################################
 
+from torch.optim import lr_scheduler
 
 @dataclass
-class OptimizerConfig:
-    optimizer: str = 'AdamW'
-    params: t.List[t.Dict[str,  t.Union[t.List[str], float]]] = None
-    lr0: float=1e-4
-    weight_decay: float=0
-    eps: float = 1e-8
-    betas: t.Tuple[float, float] = (0.9, 0.999)
+class SchedulerConfig:
+    scheduler: str = 'ReduceLROnPlateau'
+    mode: str = 'min'
+    factor: float=0.1
+    patience: int = 10
 
 
 ###############################################################################
-# OPTIMIZER REPOSITORY
+# SCHEDULER REPOSITORY
 ###############################################################################
 from pathlib import Path
 import yaml
 
 
-class OptimizerRepository:
-    optimizer_FN: str = 'optimizer'
+class SchedulerRepository:
+    scheduler_FN: str = 'scheduler'
     CONFIG_FN: str = 'configs.yaml'
 
     def __init__(
         self,
-        optimizer_folder: str,
-        optimizer_fn: str = optimizer_FN,
+        scheduler_folder: str,
+        scheduler_fn: str = scheduler_FN,
         config_fn: str = CONFIG_FN,
     ) -> None:
-        self.optimizer_fn = optimizer_fn
+        self.scheduler_fn = scheduler_fn
         self.config_fn = config_fn
 
         self._folder: Path = None
-        self._optimizer_fp: Path = None
+        self._scheduler_fp: Path = None
         self._conf_fp: Path = None
-        self.set_optimizer_folder(optimizer_folder)
+        self.set_optimizer_folder(scheduler_folder)
 
     def set_optimizer_folder(self, folder: str) -> None:
         """
         Set the optimizer storage folder and update all file paths.
 
-        :param folder: Root directory path for optimizer storage.
+        :param folder: Root directory path for scheduler storage.
         """
         self._folder = Path(folder)
-        self._optimizer_fp = self._folder / (self.optimizer_fn + '.pth')
+        self._scheduler_fp = self._folder / (self.scheduler_fn + '.pth')
         self._conf_fp = self._folder / self.config_fn
 
     @staticmethod
     def save_weights(m: optim.Optimizer, file_path: Path) -> None:
         """
-        Save optimizer weights to specified file path.
+        Save scheduler weights to specified file path.
 
-        :param m: PyTorch optimizer module to save.
+        :param m: PyTorch scheduler module to save.
         :param file_path: Destination file path for weights.
         """
         file_path.parent.mkdir(exist_ok=True)
@@ -91,7 +91,7 @@ class OptimizerRepository:
 
         :param file_path: Source file path for weights.
         :param map_location: Device to load weights onto, defaults to CPU.
-        :return: Dictionary containing optimizer state.
+        :return: Dictionary containing scheduler state.
         :raises FileNotFoundError: If weights file does not exist.
         """
         if isinstance(file_path, str):
@@ -133,98 +133,76 @@ class OptimizerRepository:
             file_path = Path(file_path)
         if not file_path.is_file():
             raise FileNotFoundError(
-                "No such optimizer file at \"%s\"." % (str(file_path),)
+                "No such scheduler file at \"%s\"." % (str(file_path),)
             )
         with open(file=file_path, mode='r', encoding='utf-8') as file:
             config = yaml.safe_load(file)
             return config
 
-    def save(self, opt: optim.Optimizer, config: OptimizerConfig) -> None:
-        self.save_weights(opt, self._optimizer_fp)
+    def save(self, scheduler, config: SchedulerConfig) -> None:
+        self.save_weights(scheduler, self._scheduler_fp)
         self.save_config(config.__dict__, self._conf_fp)
 
-    def load_optimizer(self, opt: optim.Optimizer) -> optim.Optimizer:
-        weights = self.load_weights(self._optimizer_fp)
-        opt.load_state_dict(weights)
-        return opt
+    def load_scheduler(self, scheduler):
+        weights = self.load_weights(self._scheduler_fp)
+        scheduler.load_state_dict(weights)
+        return scheduler
 
-    def load_optimizer_config(self, config: OptimizerConfig) -> OptimizerConfig:
+    def load_optimizer_config(self, config: SchedulerConfig) -> SchedulerConfig:
         config_dict = self.load_config(self._conf_fp)
         config.__dict__.update(config_dict)
         return config
 
 
 ###############################################################################
-# OPTIMIZER FACTORY
+# SCHEDULER FACTORY
 ###############################################################################
-import re
 
 
-def _find_param(pattern: str, model: nn.Module) -> nn.Parameter:
-    found_params = []
-    for name, param in model.named_parameters():
-        if re.match(pattern=pattern, string=name):
-            found_params.append(param)
-    return found_params
-
-
-def _adamw_optim_factory_fn(model: nn.Module, config: OptimizerConfig):
-    params_list = []
-    if config.params is not None:
-        for param_conf in config.params:
-            names = param_conf['params']
-            found_params = []
-            for name in names:
-                found = _find_param(name, model)
-                found_params.extend(found)
-            params_list.append(
-                {'params': found_params, 'lr': param_conf['lr']}
-            )
-    else:
-        param_conf = list(model.parameters())
-
-    assert params_list, "The model parameters list provided is empty."
-    instance = optim.AdamW(
-        params=params_list, lr=config.lr0, weight_decay=config.weight_decay,
-        eps=config.eps, betas=config.betas,
+def _reducelronplateau_factory_fn(
+    optimizer: optim.Optimizer, config: SchedulerConfig
+):
+    instance = lr_scheduler.ReduceLROnPlateau(
+        optimizer=optimizer, mode=config.min, factor=config.factor,
+        patience=config.patience,
     )
     return instance
 
 
-IMPLEMENTED_OPTIMIZERS = {
-    'AdamW': _adamw_optim_factory_fn,
+IMPLEMENTED_SCHEDULERS = {
+    'ReduceLROnPlateau': _reducelronplateau_factory_fn,
 }
 
 
-class OptimizerFactory:
+class SchedulerFactory:
 
     @staticmethod
     def build(
-        model: nn.Module,
-        config: OptimizerConfig=None,
+        optimizer: optim.Optimizer,
+        config: SchedulerConfig=None,
         **kwargs: t.Dict[str, t.Any]
-    ) -> optim.Optimizer:
+    ):
         if config is None:
-            config = OptimizerConfig()
+            config = SchedulerConfig()
         config.__dict__.update(kwargs)
-        if config.optimizer not in IMPLEMENTED_OPTIMIZERS:
+        if config.scheduler not in IMPLEMENTED_SCHEDULERS:
             raise NotImplementedError(
-                "The optimizer named '%s' is not implemented yed."
-                % (config.optimizer,)
+                "The scheduler named '%s' is not implemented yed."
+                % (config.scheduler,)
             )
-        optim_factory_fn = IMPLEMENTED_OPTIMIZERS[config.optimizer]
-        instance = optim_factory_fn(model, config)
+        optim_factory_fn = IMPLEMENTED_SCHEDULERS[config.scheduler]
+        instance = optim_factory_fn(optimizer, config)
         return instance
-
+    
     @staticmethod
     def load(
-        repository: OptimizerRepository
-    ) -> t.Tuple[optim.Optimizer, OptimizerConfig]:
-        model_config = OptimizerConfig()
-        model_config = repository.load_model_config(model_config)
-        model = OptimizerFactory.build(model_config)
-        loaded_model = repository.load_model(model)
-        return loaded_model, model_config
+        repository: SchedulerRepository
+    ):
+        scheduler_config = SchedulerConfig()
+        scheduler_config = repository.load_scheduler_config(scheduler_config)
+        scheduler = SchedulerFactory.build(scheduler_config)
+        loaded_scheduler = repository.load_scheduler(scheduler)
+        return loaded_scheduler, scheduler_config
 
 
 ###############################################################################
@@ -235,44 +213,60 @@ def _get_arguments():
     import argparse
 
     parser = argparse.ArgumentParser()
-    # optimizer: str = 'AdamW'
-    # params: t.List[t.Dict[str,  t.Union[t.List[str], float]]] = None
-    # lr0: float=1e-4
-    # weight_decay: float=0
-    # eps: float = 1e-8
-    # betas: t.Tuple[float, float] = (0.9, 0.999)
-    optim_choices = list(IMPLEMENTED_OPTIMIZERS.keys())
+
+    # scheduler: str = 'ReduceLROnPlateau'
+    # mode: str = 'min'
+    # factor: float=0.5
+    # patience: int = 10
+    scheduler_choices = list(IMPLEMENTED_SCHEDULERS.keys())
     parser.add_argument(
-        '--optimizer', type=str, choices=optim_choices, default='AdamW',
+        '--scheduler-name', type=str, choices=scheduler_choices,
+        default='ReduceLROnPlateau',
         help=(
-            "The name of the optimizer selected or the path to file "
-            "of the saved optimizer. By default `AdamW` is selected."
+            "The name of the scheduler selected or the path to file "
+            "of the saved scheduler. By default `ReduceLROnPlateau` "
+            "is selected."
         )
     )
     parser.add_argument(
-        '--params', type=str, default=None,
-        help="The config file of the leyer parameter optimizations."
+        '--mode', type=str, choices=["min", "max"], default="min",
+        help=(
+            "One of min, max. In min mode, `lr` will be reduced "
+            "when the quantity monitored has stopped decreasing; in max mode "
+            "it will be reduced when the quantity monitored has stopped "
+            "increasing. Default: `'min'`."
+        )
     )
     parser.add_argument(
-        '--lr0', type=float, default=1e-4,
-        help="The learning rate of the optimization step."
+        '--factor', type=float, default=0.1,
+        help=(
+            "Factor by which the learning rate will be reduced. "
+            "`new_lr = lr * factor`. Default: `0.1`."
+        )
     )
     parser.add_argument(
-        '--weight_decay', type=float, default=0,
-        help="The weight decay value of the optimizer."
-    )
-    parser.add_argument(
-        '--eps', type=float, default=1e-8,
-        help="The epsilon value of the optimizer."
-    )
-    parser.add_argument(
-        '--betas', nargs=2, type=float, default=(0.9, 0.999),
-        help="The beta values of the optimizer."
+        '--patience', type=int, default=10,
+        help=(
+            "The number of allowed epochs with no improvement "
+            "after which the learning rate will be reduced. "
+            "For example, consider the case of having no patience "
+            "(patience = 0). In the first epoch, a baseline is established "
+            "and is always considered good as there's no previous baseline. "
+            "In the second epoch, if the performance is worse "
+            "than the baseline, we have what is considered an intolerable "
+            "epoch. Since the count of intolerable epochs (1) "
+            "is greater than the patience level (0), the learning rate "
+            "is reduced at the end of this epoch. From the third epoch "
+            "onwards, the learning rate continues to be reduced at the end "
+            "of each epoch if the performance is worse than the baseline. "
+            "If the performance improves or remains the same, "
+            "the learning rate is not adjusted. Default: 10."
+        )
     )
 
     parser.add_argument(
-        '-m', '--model', type=str,
-        help="The path to the saved model weights."
+        '--optimizer', type=str, default=None, 
+        help="The path to the saved optimizer what we want to schedule."
     )
     parser.add_argument(
         '-o', '--output', type=str, default=None,
@@ -284,11 +278,11 @@ def _get_arguments():
     return parser.parse_args()
 
 
-def _build_optimizer() -> None:
+def _build_scheduler() -> None:
     ...
 
 
-def _load_optimizer() -> None:
+def _load_scheduler() -> None:
     ...
 
 
@@ -299,11 +293,12 @@ def main() -> None:
 
     args = _get_arguments()
     output_dir = args.output if args.output is not None else ''
-    saved_optimizer = None
+    saved_scheduler = None
 
-    if os.path.isdir(args.optimizer):
-        saved_optimizer = Path(args.optimizer)
+    if os.path.isdir(args.scheduler):
+        saved_scheduler = Path(args.scheduler)
 
+    sys.exit(0)
 
 if __name__ == '__main__':
     main()
