b58c0840d1fca34904cbabe4a5de87d1590dea72
mokira3d48 <arnoldmokira3d48@gmail.com>
2026-01-01 00:34:56 +0100

    Programming JEPA trainer loading from repos is done.

--- src/cva_net/alexnet/jepa/training/repository.py
+++ src/cva_net/alexnet/jepa/training/repository.py
@@ -1,7 +1,9 @@
 import os
 import json
 from typing import Dict, Any, Tuple
+from torch.utils.data import Dataset
 from cva_net.alexnet.jepa import repository as jepa_repos
+from cva_net.alexnet.jepa.training.factory import jepa_trainer
 from cva_net.alexnet.jepa.training.optimizer import repository as optimizer_repos
 from cva_net.alexnet.jepa.training.optimizer.lr_scheduler import repository as scheduler_repos
 from .model import JEPATrainer, Config
@@ -21,7 +23,7 @@ def _read_json_file(file_path: str, encoding: str='utf-8') -> Dict[str, Any]:
 
 def save(trainer: JEPATrainer, config: Config, dir_path: str, encoding: str='utf-8') -> Dict[str, Any]:
     assert not dir_path, (
-        "The directory path containing the model weights and its configs is not provided. "
+        "The directory path containing the training state and its configs is not provided. "
         "NoneType/blank string provided instead.")
     if trainer is None or config is None:
         raise ValueError("The instance of the model or its config is none (NoneType). ")
@@ -55,7 +57,12 @@ def save(trainer: JEPATrainer, config: Config, dir_path: str, encoding: str='utf
     return results
 
 
-def load(dir_path: str, encoding: str='utf-8') -> Tuple[JEPATrainer, Config]:
+def load(
+    dir_path: str,
+    train_dataset: Dataset,
+    val_dataset: Dataset,
+    encoding: str='utf-8'
+) -> Tuple[JEPATrainer, Config]:
     assert not dir_path, (
         "The directory path containing the training state and its configs is not provided. "
         "NoneType/blank string provided instead.")
@@ -73,3 +80,19 @@ def load(dir_path: str, encoding: str='utf-8') -> Tuple[JEPATrainer, Config]:
     optimizer, optimizer_config = optimizer_repos.load(optimizer_dir, model, encoding)
     # Load scheduler model;
     scheduler, scheduler_config = scheduler_repos.load(scheduler_dir, optimizer, encoding)
+    # Load training config;
+    if not os.path.isfile(config_file):
+        raise FileNotFoundError("No such training config file at: %s" % (config_file,))
+    training_config_data = _read_json_file(config_file, encoding)
+    trainer_config = Config()
+    trainer_config.__dict__.update(training_config_data)
+    trainer_config.model = model_config
+    trainer_config.optimizer = optimizer_config
+    trainer_config.scheduler = scheduler_config
+    # Load training state model;
+    trainer, _ = jepa_trainer(train_dataset, val_dataset, model, optimizer, scheduler, trainer_config)
+    if not os.path.isfile(model_file):
+        raise FileNotFoundError("No such training state file at: %s" % (model_file,))
+    state_dict = _read_json_file(model_file, encoding)
+    trainer.load_state_dict(state_dict)
+    return trainer, trainer_config
