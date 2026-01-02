ced924ec7542974a3fc981d49f80554d4aaf99e7
mokira3d48 <arnoldmokira3d48@gmail.com>
2026-01-02 19:27:08 +0100

    Update of training process of JEPA.

--- src/cvanet/alexnet/jepa/training/model.py
+++ src/cvanet/alexnet/jepa/training/model.py
@@ -50,7 +50,7 @@ class JEPATrainer:
         self.model: JEPA = None
         self.optimizer: Optimizer = None
         self.scheduler: LRScheduler = None
-        self._device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self._device = None
         self._mon: Monitor = Monitor()
         self._train_dataset_loader: DataLoader = None
         self._val_dataset_loader: DataLoader = None
@@ -72,6 +72,7 @@ class JEPATrainer:
         epoch = self._checkpoint_manager.get_latest_checkpoint()
         if not epoch:
             return None
+        self._mon.log("Checkpoint found.")
         self._config = self._checkpoint_manager.load_config()
         self._start_epoch_idx = epoch
         self._checkpoint_loaded = True
@@ -85,7 +86,15 @@ class JEPATrainer:
         assert self.scheduler is None or self._config.scheduler is not None, "The scheduler config is not specified."
         # Device setting;
         if self._config.device:
-            self._device = torch.device(self._config.device)
+            device_name = self._config.device
+            if device_name.startswith('cuda'):
+                if not torch.cuda.is_available():
+                    self._mon.log(f"The device named \"{device_name}\" is not available. So we select CPU device.")
+                    device_name = 'cpu'
+            self._device = torch.device(device_name)
+        else:
+            self._device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self._mon.log(f"Device selected is \"{self._device}\".")
         # Create dataloaders;
         use_pin_memory = False
         if self._device.type == 'gpu':
@@ -96,22 +105,35 @@ class JEPATrainer:
             pin_memory=use_pin_memory)
         self._train_dataset_loader = dataloaders[0]
         self._val_dataset_loader = dataloaders[1]
+        self._mon.log("Training dataset:")
+        self._mon.log(f"  Number of batchs -> {len(self._train_dataset_loader)}")
+        self._mon.log("Validation dataset:")
+        self._mon.log(f"  Number of batchs -> {len(self._val_dataset_loader)}")
         # Instanciation of the model;
         if self.model is None:
             if self._config.model is None:
                 self._config.model = JEPAConfig()
             self.model, _ = jepa(self._config.model)
             self.model = self.model.to(self._device)
+        model_stat = summary.build(self.model, device=self._device)
+        self._mon.log("=" * 120)
+        self._mon.log("MODEL SUMMARY")
+        self._mon.log(f"{"=" * 120}")
+        self._mon.log(f"\n{model_stat}")
         # Instanciation of optimizer model;
         if self.optimizer is None:
             if self._config.optimizer is None:
                 self._config.optimizer = OptimizerConfig()
             self.optimizer = build_optimizer(self.model, self._config.optimizer)
+        self._mon.log("Optimizer:")
+        self._mon.log("  Config: " + repr(self.optimizer))
         # Instanciation of scheduler model;
         if self.scheduler is None:
             if self._config.scheduler is None:
                 self._config.scheduler = LRSchedulerConfig()
             self.scheduler = lr_scheduler(self.optimizer, self._config.scheduler)
+        self._mon.log("Scheduler:")
+        self._mon.log("  Config: " + repr(self.scheduler))
         if self._checkpoint_loaded:
             self._checkpoint_manager.load_data(self._config, self, self._device)
         # Specify that all is ready;
@@ -130,7 +152,7 @@ class JEPATrainer:
         self._best_val_loss = state_dict['best_val_loss']
         self._history.load_state_dict(state_dict['history'])
 
-    def train_epoch(self):
+    def train_epoch(self) -> Dict[str, Any]:
         """
         Training function on one epoch.
         """
@@ -184,7 +206,7 @@ class JEPATrainer:
             'cosine_loss': avg_cosine,
         }
 
-    def validate(self):
+    def validate(self) -> Dict[str, Any]:
         """
         Validation function on one epoch.
         """
@@ -228,12 +250,6 @@ class JEPATrainer:
     def execute(self, num_epochs: int=1) -> History:
         assert self._compiled is True, (
             "You must call the method called `compile()` in first, before call the `execute()` method.")
-        # Print model summary;
-        model_stat = summary.build(self.model)
-        self._mon.log("="* 120)
-        self._mon.log("MODEL SUMMARY")
-        self._mon.log(f"{"=" * 120}\n")
-        self._mon.log(f"\n{model_stat}")
         # Training;
         self._mon.log(f"{'=' * 120}")
         self._mon.log(f"STARTING OF JEPA TRAINING - {num_epochs} EPOCHS")
